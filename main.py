9bcdabe6968b8b95ba01916b7e6f584640447f73
shahafabileah <shahaf@gmail.com>
2024-03-22 23:47:46 -0700

    Adding spotify using their API

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import os
 import requests
 import re
 
 def list_songs():
   print("platform, song, url")
   list_apple_music_songs()
-  # list_spotify_songs()
+  list_spotify_songs()
 
 def list_apple_music_songs():
   unique_song_urls = set()
@@ -30,10 +31,61 @@ def list_apple_music_songs():
     song_name = url.split('/')[-2].replace('-', ' ').title()
     print(f"apple music, {song_name}, {url}")
 
-# def list_spotify_songs():
-#   print("spotify, song1, https://open.spotify.com/track/0y4TKcc7p2H6P0GJlt01EI")
-#   print("spotify, song2, https://open.spotify.com/track/0y4TKcc7p2H6P0GJlt01EI")
-#   print("spotify, song3, https://open.spotify.com/track/0y4TKcc7p2H6P0GJlt01EI")
+def list_spotify_songs():
+  # Getting started docs:
+  # https://developer.spotify.com/documentation/web-api/tutorials/getting-started#create-an-app
+  # 
+  # I registered this app:
+  # https://developer.spotify.com/dashboard/92ab56cdd37e4092b1ac031eae3efabc
+
+  client_id = '92ab56cdd37e4092b1ac031eae3efabc'
+  # Get secret from environment variable
+  client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+
+  # Use the client ID and secret to get an access token
+  token_url = 'https://accounts.spotify.com/api/token'
+  token_params = {
+    'grant_type': 'client_credentials',
+    'client_id': client_id,
+    'client_secret': client_secret 
+  }
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  token_response = requests.post(token_url, data=token_params, headers=headers)
+  token = token_response.json()['access_token']
+
+  headers = {
+    'Authorization': f'Bearer {token}'
+  }
+
+  # Get all album IDs
+  album_ids = []
+  url = 'https://api.spotify.com/v1/artists/6E9qceaxgzpMz5ecZfni6O/albums?limit=50'
+  while True:
+    albums = requests.get(url, headers=headers)
+    for album in albums.json()['items']:
+      album_ids.append(album['id'])
+
+    if albums.json()['next']:
+      url = albums.json()['next']
+    else:
+      break
+
+  # For each album, get a list of tracks
+  for album_id in album_ids:
+    url = f'https://api.spotify.com/v1/albums/{album_id}/tracks?limit=50'
+    while True:
+      tracks = requests.get(url, headers=headers)
+      for track in tracks.json()['items']:
+        song_name = track['name']
+        song_url = track['external_urls']['spotify']
+        print(f"spotify, \"{song_name}\", {song_url}")
+      
+      if tracks.json()['next']:
+        url = tracks.json()['next']
+      else:
+        break
 
 if __name__ == "__main__":
   list_songs()
